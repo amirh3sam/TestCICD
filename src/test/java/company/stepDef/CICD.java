6f78cb2a90d36e47401cc1528fd55148d30a2d12
AmirHesamB <amirh3sam.badiei@gmail.com>
2023-07-26 15:13:25 -0400

    add assert

--- src/test/java/company/stepDef/CICD.java
+++ src/test/java/company/stepDef/CICD.java
@@ -1,6 +1,7 @@
 package company.stepDef;
 
 import io.cucumber.java.en.Given;
+import org.junit.Assert;
 
 public class CICD {
 
@@ -8,6 +9,7 @@ public class CICD {
     @Given("say hello CICD")
     public void say_hello_cicd() {
         System.out.println("Hello CI/CD");
+        Assert.assertEquals(2,2);
     }
 
 }
